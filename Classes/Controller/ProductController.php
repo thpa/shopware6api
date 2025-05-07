a2f75c3b7e27e5988d54ada17b5fcc021266f411
Thomas Paul <thomas@webschmiede.at>
2025-05-07 10:28:20 +0200

    fix(frontend): update controller actions for TYPO3 v13
    - Add ResponseInterface return type to controller actions
    - Return htmlResponse() from actions
    - Fix "did not return ResponseInterface" error
    
    This makes the controller compatible with TYPO3 v13's
    PSR-7 response handling requirements.

--- Classes/Controller/ProductController.php
+++ Classes/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace ThomasPaul\Shopware6Api\Controller;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use ThomasPaul\Shopware6Api\Domain\Repository\ProductRepository;
 use ThomasPaul\Shopware6Api\Domain\Model\Product;
+use Psr\Http\Message\ResponseInterface;
 
 class ProductController extends ActionController
 {
@@ -14,14 +15,18 @@ class ProductController extends ActionController
         $this->productRepository = $productRepository;
     }
 
-    public function listAction()
+    public function listAction(): ResponseInterface
     {
         $products = $this->productRepository->findAll();
         $this->view->assign('products', $products);
+        
+        return $this->htmlResponse();
     }
 
-    public function showAction(Product $product)
+    public function showAction(Product $product): ResponseInterface
     {
         $this->view->assign('product', $product);
+        
+        return $this->htmlResponse();
     }
 } 
